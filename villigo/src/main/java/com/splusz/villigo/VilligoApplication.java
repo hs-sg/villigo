ec6e32e5a41e58b9f7acef22100b910164cfb5fb
hs-sg <hasin927@gmail.com>
2025-06-03 20:32:10 +0900

    Dockerfile 힙설정 수정-용량축소, JVM 메모리 로그 코드 추가

--- villigo/src/main/java/com/splusz/villigo/VilligoApplication.java
+++ villigo/src/main/java/com/splusz/villigo/VilligoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import jakarta.annotation.PostConstruct;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class VilligoApplication {
@@ -11,5 +13,12 @@ public class VilligoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VilligoApplication.class, args);
 	}
-
+	
+	@PostConstruct
+	public void printJvmInfo() {
+		long maxMem = Runtime.getRuntime().maxMemory() / (1024*1024);
+		long totalMem = Runtime.getRuntime().totalMemory() / (1024*1024);
+	    System.out.println("🧠 JVM Max Memory: " + maxMem + " MB");
+	    System.out.println("💾 JVM Total Memory: " + totalMem + " MB");
+	}
 }
