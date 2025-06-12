3474189e2906e7e40e74987049df0fe8dab9ea60
hs-sg <hasin927@gmail.com>
2025-06-12 11:32:17 +0900

    S3Config 수정

--- villigo/src/main/java/com/splusz/villigo/config/S3Config.java
+++ villigo/src/main/java/com/splusz/villigo/config/S3Config.java
@@ -6,24 +6,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
-@ConfigurationProperties(prefix = "aws")
+//@ConfigurationProperties(prefix = "aws")
 public class S3Config {
 	
-    private String accessKey;
-    private String secretKey;
-    private String region;
+//    private String accessKey;
+//    private String secretKey;
+//    private String region;
+    
+//    @Bean
+//    public S3Client s3Client() {
+//        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+//        return S3Client.builder()
+//                .region(Region.of(region))
+//                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+//                .build();
+//    }
     
     @Bean
     public S3Client s3Client() {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
