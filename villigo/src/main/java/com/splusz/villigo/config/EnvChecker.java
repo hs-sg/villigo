b33a34f848e22b46947a6bfb2eca2de70a617656
hs-sg <hasin927@gmail.com>
2025-06-19 18:44:10 +0900

    EnvChecker 수정

--- villigo/src/main/java/com/splusz/villigo/config/EnvChecker.java
+++ villigo/src/main/java/com/splusz/villigo/config/EnvChecker.java
@@ -1,16 +1,14 @@
 package com.splusz.villigo.config;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
-public class EnvChecker implements ApplicationRunner {
-    @Override
-    public void run(ApplicationArguments args) {
-        System.out.println("△△△ SUPABASE_URL: " + System.getenv("SUPABASE_URL"));
-        System.out.println("△△△ SUPABASE_USERNAME: " + System.getenv("SUPABASE_USERNAME"));
-        System.out.println("△△△ SUPABASE_PW: " + System.getenv("SUPABASE_PW"));
+@Configuration
+public class EnvChecker {
+    static {
+        System.out.println("◇ [STATIC] SUPABASE_URL: " + System.getenv("SUPABASE_URL"));
+        System.out.println("◇ [STATIC] SUPABASE_USERNAME: " + System.getenv("SUPABASE_USERNAME"));
+        System.out.println("◇ [STATIC] SUPABASE_PW: " + System.getenv("SUPABASE_PASSWORD"));
     }
 }
+
 
