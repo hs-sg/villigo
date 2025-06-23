3608fce9c000a9bcf7561dbdbfb0cf57db9e1dda
hs-sg <hasin927@gmail.com>
2025-06-23 16:26:51 +0900

    entrypoint, Dockerfile, application.properties 수정

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,11 @@
 FROM gradle:8.7.0-jdk21 AS builder
 WORKDIR /app
 COPY . .
-RUN ./gradlew clean build -x test -x check
+RUN chmod +x gradlew && ./gradlew clean build -x test -x check
 
 # 런타임 단계
 FROM eclipse-temurin:21-jdk
 WORKDIR /app
-
-# 환경변수를 받을 준비
-ENV SUPABASE_URL
-ENV SUPABASE_USERNAME
-ENV SUPABASE_PASSWORD
 
 # JAR 및 entrypoint.sh 복사
 COPY --from=builder /app/build/libs/*.jar app.jar
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,6 @@ echo "==== ENV CHECK ===="
 echo "SUPABASE_URL: $SUPABASE_URL"
 echo "SUPABASE_USERNAME: $SUPABASE_USERNAME"
 echo "SUPABASE_PASSWORD: $SUPABASE_PASSWORD"
+echo "===================="
 
-exec java -Xmx768m -Xms384m \
-  -Dspring.datasource.url=$SUPABASE_URL \
-  -Dspring.datasource.username=$SUPABASE_USERNAME \
-  -Dspring.datasource.password=$SUPABASE_PASSWORD \
-  -jar app.jar
+exec java -Xmx768m -Xms384m -jar app.jar
